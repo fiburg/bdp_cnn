bf41d64b601e4a34535ec4fa2d0a3ab8dd181136
Tobias Machnitzki <darklefknight@googlemail.com>
2018-03-08 20:57:49 +0100

    Got first results from LSTM Model. Best try so far RMSE of 0.6. With this commit comes some pictures showing truth and Machinlearning prognostics with RMSE of 1.051. Run with one layer and 20 Neurons. Avg. Runtim ca. 8 minutes.

--- bdp_cnn/Lorenz/CNN_Lorenz.py
+++ bdp_cnn/Lorenz/CNN_Lorenz.py
@@ -1,18 +1,31 @@
 from keras.models import Sequential
-from keras.layers import Dense,MaxPool1D,Dropout
+from keras.layers import Dense,MaxPool1D,Dropout,LSTM
 import numpy as np
 import matplotlib.pyplot as plt
 from netCDF4 import Dataset
 import pandas
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from keras.callbacks import ModelCheckpoint
 
 class CNN(object):
 
-    def __init__(self, x=None, y=None):
+    def __init__(self, x=None, y=None, split=10000):
         self.model = None
         self.x_train = x
         self.y_train = y
         self.data = None
+        self.scaled = None
+        self.scaler = 1
+        self.test = None
+        self.train = None
+        self.train_scaled = None
+        self.test_scaled = None
+        self.supervised_values = None
+        self.raw_values = None
+        self.bach_size = 1
+        self.split = -split
+
 
     def run_model(self):
 
@@ -73,14 +86,17 @@ class CNN(object):
 
         self.x_train = x[0,:,0] # pass only one gridpoint for now
 
-    def prepare_data_1_member(self):
-        self.data = timeseries_to_supervised(self.x_train)
-        X = self.data.values[:,1]
-        X = X.reshape(len(X), 1)
+    def prepare_data_1_member(self,train=None):
+        if not train:
+            train = self.x_train
+        self.data = self.timeseries_to_supervised(train)
+        self.X = self.data.values[:,1]
+        self.X = self.X.reshape(len(self.X), 1)
         scaler = MinMaxScaler(feature_range=(-1, 1))
-        scaler = scaler.fit(X)
-        scaled_X = scaler.transform(X)
+        scaler = scaler.fit(self.X)
+        scaled_X = scaler.transform(self.X)
         scaled_series = pandas.Series(scaled_X[:, 0])
+        self.scaled = scaled_series
         print(scaled_series.head())
         # invert transform
         inverted_X = scaler.inverse_transform(scaled_X)
@@ -88,39 +104,125 @@ class CNN(object):
         print(inverted_series.head())
 
 
+    def init_lstm(self, batch_size=None, nb_epoch=5, neurons=5):
+        if not batch_size:
+            batch_size = self.bach_size
+        else:
+            self.bach_size = batch_size
+        X, y = self.train[:, 0:-1], self.train[:, -1]
+        X = X.reshape(X.shape[0], 1, X.shape[1])
+        model = Sequential()
+        model.add(LSTM(neurons, batch_input_shape=(batch_size, X.shape[1], X.shape[2]), stateful=True))
+        model.add(Dense(1))
+        model.compile(loss='mean_squared_error', optimizer='adam')
+        checkpoint = ModelCheckpoint("weights.{epoch:02d}.hdf5", monitor='val_loss',
+                                     save_best_only=False,
+                                     save_weights_only=False, mode='auto', period=1)
+        callback_list = [checkpoint]
+        for i in range(nb_epoch):
+            model.fit(X, y, epochs=1, batch_size=batch_size, shuffle=False,callbacks=callback_list)
+            model.reset_states()
+        self.model = model
+        self.train_reshaped = self.train_scaled[:, 0].reshape(len(self.train_scaled), 1, 1)
+
+    def predict(self):
+
+        self.model.predict(self.train_reshaped, batch_size=self.bach_size)
+
+    def scale(self):
+        # fit scaler
+        self.scaler = MinMaxScaler(feature_range=(-1, 1))
+        self.scaler = self.scaler.fit(self.train)
+        # transform train
+        self.train = self.train.values.reshape(self.train.shape[0], self.train.shape[1])
+        self.train_scaled = self.scaler.transform(self.train)
+        # transform test
+        self.test = self.test.values.reshape(self.test.shape[0], self.test.shape[1])
+        self.test_scaled = self.scaler.transform(self.test)
+
+
+    def invert_scale(self, X, value):
+        new_row = [x for x in X] + [value]
+        array = np.array(new_row)
+        array = array.reshape(1, len(array))
+        inverted = self.scaler.inverse_transform(array)
+        return inverted[0, -1]
+
+    def create_train_test(self):
+        self.train, self.test = self.supervised_values[0:self.split], self.supervised_values[-self.split:]
+
+
+    def walk_forward_validation(self):
+
+        def forecast_lstm(model, batch_size, X):
+            X = X.reshape(1, 1, len(X))
+            yhat = model.predict(X, batch_size=batch_size)
+            return yhat[0, 0]
+
+        self.predictions = list()
+        for i in range(len(self.test_scaled)):
+            # make one-step forecast
+            X, y = self.test_scaled[i, 0:-1], self.test_scaled[i, -1]
+            yhat = forecast_lstm(self.model, 1, X)
+            # invert scaling
+            yhat = self.invert_scale(X, yhat)
+            # store forecast
+            self.predictions.append(yhat)
+            # expected = self.raw_values[len(self.train) + i + 1]
+            # print('Hour=%d, Predicted=%f, Expected=%f' % (i + 1, yhat, expected))
+
+
+    def make_supervised(self, lag=1):
+        """
+        Takes a numpy array (1D!!!) as input and creates a second array which lacks the input array behind.
+        The array lagging behind will be used as input data for the cnn while the other will be
+        considered the "Truth".
+
+        Args:
+            data: numpy-array (2D).
+            lag: int: number of steps one array should lag behind the other.
+
+        Returns:
+            pandas dataframe containing the original and the lagging timeseries.
+        """
+
+        df = pandas.DataFrame(self.x_train)
+        self.raw_values = df.values
+        columns = [df.shift(i) for i in range(1, lag + 1)]
+        columns.append(df)
+        df = pandas.concat(columns, axis=1)
+        df.fillna(0, inplace=True)
+        self.supervised_values = df
+
+    def report_performance(self):
+
+        rmse = np.sqrt(mean_squared_error(self.raw_values[-self.split:], self.predictions))
+        print('Test RMSE: %.3f' % rmse)
+        # line plot of observed vs predicted
+        fig,ax = plt.subplots()
+        ax.plot(self.raw_values[-self.split:],label="Truth")
+        ax.plot(self.predictions,label="Machine learning")
+        ax.legend(loc="upper left")
+        plt.savefig("complete.png")
+
+        fig,ax = plt.subplots()
+        ax.plot(self.raw_values[-self.split:],label="Truth")
+        ax.plot(self.predictions,label="Machine learning")
+        ax.set_xlim(0,10000)
+        ax.legend(loc="upper left")
+        plt.savefig("last_10k.png")
+
+        fig, ax = plt.subplots()
+        ax.plot(self.raw_values[-self.split:],label="Truth")
+        ax.plot(self.predictions,label="Machine learning")
+        ax.set_xlim(0, 1000)
+        ax.legend(loc="upper left")
+        plt.savefig("last_1k.png")
 
 
 
-def timeseries_to_supervised(data, lag=1):
-    """
-    Takes a numpy array (1D!!!) as input and creates a second array which lacks the input array behind.
-    The array lagging behind will be used as input data for the cnn while the other will be
-    considered the "Truth".
 
-    Args:
-        data: numpy-array (2D).
-        lag: int: number of steps one array should lag behind the other.
 
-    Returns:
-        pandas dataframe containing the original and the lagging timeseries.
-    """
-
-    df = pandas.DataFrame(data)
-    columns = [df.shift(i) for i in range(1, lag + 1)]
-    columns.append(df)
-    df = pandas.concat(columns, axis=1)
-    df.fillna(0, inplace=True)
-    return df
-
-def difference(dataset, interval=1):
-	diff = list()
-	for i in range(interval, len(dataset)):
-		value = dataset[i] - dataset[i - interval]
-		diff.append(value)
-	return pandas.Series(diff)
-
-def inverse_difference(history, yhat, interval=1):
-	return yhat + history[-interval]
 
 if __name__ == "__main__":
     from bdp_cnn.Lorenz.DataCreation1 import creation_main
@@ -136,9 +238,20 @@ if __name__ == "__main__":
     # results = np.subtract(tested[:,0], truth)
     # plt.plot(results)
 
-
+    import timeit
+    start = timeit.default_timer()
     cnn = CNN()
     cnn.read_netcdf("100_years_1_member.nc")
-    cnn.prepare_data_1_member()
+    cnn.make_supervised()
+    cnn.create_train_test()
+    cnn.scale()
+    cnn.init_lstm(batch_size=1,nb_epoch=1,neurons=20)
+    cnn.predict()
+    cnn.walk_forward_validation()
+    cnn.report_performance()
+    stop = timeit.default_timer()
+    runtime = stop-start
+    print("Runtime: " )
+    print(runtime)
 
 
