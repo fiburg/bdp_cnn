3d13e2f579e78811a2b6c67af545de79934b3073
darklefknight <darklefknight@web.de>
2018-03-09 14:56:36 +0100

    changed make_train_test for 2d data changed scale for 2d data added __str__ method added lots of attributes changing everything to numpy.arrays

--- bdp_cnn/Lorenz/CNN_Lorenz.py
+++ bdp_cnn/Lorenz/CNN_Lorenz.py
@@ -11,6 +11,35 @@ from keras.callbacks import ModelCheckpoint
 class CNN(object):
 
     def __init__(self, x=None, y=None, split=10000):
+
+        self.x_raw = None
+        self.y_raw = None
+
+        # splitted dataset in original units
+        self.x_train = None
+        self.y_train = None
+        self.x_test = None
+        self.y_test = None
+        self.x_valid = None
+        self.y_valid = None
+
+        # splitted dataset scaled between -1 and 1
+        self.x_train_scaled = None
+        self.y_train_scaled = None
+        self.x_test_scaled = None
+        self.y_test_scaled = None
+        self.x_valid_scaled = None
+        self.y_valid_scaled = None
+
+        # factors used for scaling:
+        self.train_scaler = None
+        self.test_scaler = None
+        self.valid_scaler = None
+
+
+
+
+
         self.model = None
         self.data_from_netcdf = x
         self.data = None  # data
@@ -19,10 +48,10 @@ class CNN(object):
         self.train = None
         self.train_scaled = None
         self.test_scaled = None
-        self.supervised_values = None
         self.raw_values = None
         self.bach_size = 1
         self.split = -split
+
 
     def __str__(self):
         attr_list = [self.model, self.data_from_netcdf, self.data, self.scaler, self.test, self.train, self.train_scaled,
@@ -77,7 +106,7 @@ class CNN(object):
             x_tmp = x_tmp[:,:]
             x[i,:,:] = x_tmp
 
-        self.data_from_netcdf = x[0, :, :]  # pass only one gridpoint for now... [ensemble, time, grid]
+        self.data_from_netcdf = x[0, :1000, :]  # pass only one gridpoint for now... [ensemble, time, grid]
 
 
     def init_lstm(self, batch_size=None, nb_epoch=5, neurons=20):
@@ -96,11 +125,11 @@ class CNN(object):
             batch_size = self.bach_size
         else:
             self.bach_size = batch_size
-        X, y = self.train[:, 0:-1], self.train[:, -1]
+        X, y = self.x_train_scaled, self.y_train_scaled
         X = X.reshape(X.shape[0], 1, X.shape[1])
         model = Sequential()
         model.add(LSTM(neurons, batch_input_shape=(batch_size, X.shape[1], X.shape[2]), stateful=True))
-        model.add(Dense(1))
+        model.add(Dense(40))
         model.compile(loss='mean_squared_error', optimizer='adam')
         checkpoint = ModelCheckpoint("weights.{epoch:02d}.hdf5", monitor='val_loss',
                                      save_best_only=False,
@@ -110,33 +139,49 @@ class CNN(object):
             model.fit(X, y, epochs=1, batch_size=batch_size, shuffle=False,callbacks=callback_list)
             model.reset_states()
         self.model = model
-        self.train_reshaped = self.train_scaled[:, 0].reshape(len(self.train_scaled), 1, 1)
+        self.train_reshaped = self.x_train_scaled.reshape(len(self.x_train_scaled), 1, 40)
 
     def predict(self):
 
         self.model.predict(self.train_reshaped, batch_size=self.bach_size)
 
-    def scale(self):
-        # fit scaler
-        self.scaler = MinMaxScaler(feature_range=(-1, 1))
-        self.scaler = self.scaler.fit(self.train)
-        # transform train
-        self.train = self.train.values.reshape(self.train.shape[0], self.train.shape[1],self.train.shape[2])
-        self.train_scaled = self.scaler.transform(self.train)
-        # transform test
-        self.test = self.test.values.reshape(self.test.shape[0], self.test.shape[1],self.test.shape[2])
-        self.test_scaled = self.scaler.transform(self.test)
+    @staticmethod
+    def scale(array):
+        scaler = MinMaxScaler(feature_range=(-1, 1))
+        scaler = scaler.fit(array)
+        return scaler,scaler.transform(array)
 
+    def scale_all(self):
+        self.train_scaler,self.x_train_scaled = self.scale(self.x_train)
+        self.train_scaler, self.y_train_scaled = self.scale(self.y_train)
 
-    def invert_scale(self, X, value):
+        self.test_scaler,self.x_test_scaled = self.scale(self.x_test)
+        self.test_scaler, self.y_test_scaled = self.scale(self.y_test)
+
+        self.valid_scaler,self.x_valid_scaled = self.scale(self.x_valid)
+        self.valid_scaler, self.y_valid_scaled = self.scale(self.y_valid)
+
+    def invert_scale(self, X, value,scaler):
         new_row = [x for x in X] + [value]
         array = np.array(new_row)
         array = array.reshape(1, len(array))
-        inverted = self.scaler.inverse_transform(array)
+        inverted = scaler.inverse_transform(array)
         return inverted[0, -1]
 
-    def create_train_test(self):
-        self.train, self.test = self.supervised_values[0:self.split,:], self.supervised_values[-self.split:,:]
+
+    def create_train_test_validation(self):
+        """
+        splits the raw data (x_raw, y_raw) into testing, training and validation sets.
+        2/3 are training, 1/6 ist testing and 1/6 is validation.
+        """
+
+        split1 = int(len(self.x_raw)/3 * 2)
+        split2 = int(len(self.x_raw)/6 * 5)
+
+        self.x_train, self.x_test,self.x_valid = self.x_raw[:split1], self.x_raw[split1:split2], self.x_raw[split2:]
+        self.y_train, self.y_test,self.y_valid = self.y_raw[:split1], self.y_raw[split1:split2], self.y_raw[split2:]
+
+        del self.x_raw, self.y_raw
 
 
     def walk_forward_validation(self):
@@ -147,12 +192,14 @@ class CNN(object):
             return yhat[0, 0]
 
         self.predictions = list()
-        for i in range(len(self.test_scaled)):
+        for i in range(len(self.x_test_scaled)):
             # make one-step forecast
-            X, y = self.test_scaled[i, 0:-1], self.test_scaled[i, -1]
+            X = self.x_test_scaled[i,:]
             yhat = forecast_lstm(self.model, 1, X)
+            self.yhat = yhat
+            self.X = X
             # invert scaling
-            yhat = self.invert_scale(X, yhat)
+            yhat = self.invert_scale(X.reshape(1,len(X)), yhat,self.test_scaler)
             # store forecast
             self.predictions.append(yhat)
             # expected = self.raw_values[len(self.train) + i + 1]
@@ -161,26 +208,24 @@ class CNN(object):
 
     def make_supervised(self, lag=1):
         """
-        Takes a numpy array as input and creates a second array which lacks the input array behind.
+        Takes a numpy array as input (data_from_netcdf) and creates a second array which lacks the input array behind.
         The array lagging behind will be used as input data for the cnn while the other will be
         considered the "Truth".
 
+        Sets the attributes x_raw and y_raw.
+
         Args:
-            data: numpy-array (2D).
             lag: int: number of steps one array should lag behind the other.
 
-        Returns:
-            pandas dataframe containing the original and the lagging timeseries.
         """
 
-        df = pandas.DataFrame(self.data_from_netcdf)
-        self.raw_values = df.values
-        df_shift = pandas.DataFrame(np.roll(self.data_from_netcdf, lag, axis=0))
-        df = df.iloc[:, 0:40].apply(tuple, axis=1)
-        df_shift = df_shift.iloc[:, 0:40].apply(tuple, axis=1)
-        df = pandas.concat([df_shift, df], axis=1)
-        df = df.iloc[1:]
-        self.supervised_values = df
+        self.x_raw = self.data_from_netcdf.copy()
+        self.y_raw = np.roll(self.x_raw,lag,axis=0)
+        self.x_raw = np.delete(self.x_raw, 0, axis=0)
+        self.y_raw = np.delete(self.y_raw, 0, axis=0)
+
+        del self.data_from_netcdf
+
 
     def report_performance(self):
 
@@ -218,16 +263,17 @@ if __name__ == "__main__":
     start = timeit.default_timer()
     cnn = CNN()
     cnn.read_netcdf("100_years_1_member.nc")    # done
-    cnn.make_supervised()   # finn todo
-    #cnn.create_train_test()     # tobi todo
-    #cnn.scale()
-    #cnn.init_lstm(batch_size=1,nb_epoch=1,neurons=100)
-    #cnn.predict()
-    #cnn.walk_forward_validation()
-    #cnn.report_performance()
-    #stop = timeit.default_timer()
-    #runtime = stop-start
-    #print("Runtime: " )
-    #print(runtime)
+    cnn.make_supervised()   # done
+    cnn.create_train_test_validation()      # done
+    cnn.scale_all() #done
+
+    cnn.init_lstm(batch_size=1,nb_epoch=1,neurons=100)
+    cnn.predict()
+    cnn.walk_forward_validation()
+    cnn.report_performance()
+    stop = timeit.default_timer()
+    runtime = stop-start
+    print("Runtime: " )
+    print(runtime)
 
 
