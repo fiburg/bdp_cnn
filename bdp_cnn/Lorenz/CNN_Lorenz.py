7440dad4c2029c6eb4bc7f517b0fc6c4a3cbaf50
fiburg <finn.burgemeister@gmail.com>
2018-03-14 13:48:16 +0100

    skeleton

---
+++ bdp_cnn/Lorenz/CNN_Lorenz.py
@@ -0,0 +1,26 @@
+from bdp_cnn.Lorenz.NN_Lorenz import NN
+
+
+class CNN(NN):
+    def init_model(self):
+        """
+        Initialisation of neural network model
+        """
+        pass
+
+    def predict(self):
+        """
+        Prediction with model
+
+        """
+        pass
+
+    def scale(self):
+        """
+        Scale of input data
+        """
+
+    def scale_invert(self):
+        """
+        Invert application of `scale`
+        """
