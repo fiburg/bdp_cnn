adacb08595614c8a11c46da05d89f1ee72ad5664
fiburg <finn.burgemeister@gmail.com>
2018-03-15 17:04:49 +0100

    - first initialisation of model with comments to understand - TODOs: more layers, scale data between -1 and 1 otherwise replace zeropadding, ... - data preparation

--- bdp_cnn/Lorenz/CNN_Lorenz.py
+++ bdp_cnn/Lorenz/CNN_Lorenz.py
@@ -1,7 +1,8 @@
 from bdp_cnn.Lorenz.NN_Lorenz import NN
 from keras.layers import Conv2D, Dense, MaxPooling2D, Flatten
 from keras.models import Sequential
-
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 
 class CNN(NN):
     """
@@ -47,18 +48,50 @@ class CNN(NN):
         """
         pass
 
-    def scale(self):
+    def scale(self, array):
         """
         Scale of input data
         """
+        scaler = MinMaxScaler(feature_range=(-1, 1))
+        scaler = scaler.fit(array)
+        return scaler, scaler.transform(array)
 
-    def scale_invert(self):
+    def scale_invert(self, scaler, array):
         """
         Invert application of `scale`
         """
+        pass
+
+    def split_ds(self, array):
+        """
+        Split some dataset into training, testing and validation.
+
+        Args:
+            array: input data
+
+        Returns:
+            train, test, val
+
+        """
+
+        train = array[int(4/6 * array.shape[0]), :, :]
+        test = array[int(4/6 * array.shape[0]), :, :]
+        val = array[int(4/6 * array.shape[0]), :, :]
+        return train, test, val
 
     def test_cnn(self):
         model_test = self.init_model()
+        data = self.read_netcdf("100_years_1_member.nc")
+
+        x = data.copy()
+        y = np.roll(data.copy(), 1, axis=0)
+
+        xscaler, x = self.scale(x)
+        yscaler, y = self.scale(y)
+
+        xtrain, xtest, xval = self.split_ds(x)
+        ytrain, ytest, yval = self.split_ds(y)
+
 
 
 if __name__ == "__main__":
