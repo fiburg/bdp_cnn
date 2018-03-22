fa5447a0c8767e587ea37ee891dacf90f3b59be8
fiburg <finn.burgemeister@gmail.com>
2018-03-22 17:24:35 +0100

    invert scaling, solution at this point only for one time step

--- bdp_cnn/Lorenz/CNN_Lorenz.py
+++ bdp_cnn/Lorenz/CNN_Lorenz.py
@@ -21,59 +21,50 @@ class CNN(NN):
         self.filter = filter_size
         self.neurons = neurons
         self.time_steps = time_steps
-        self.init_model(nb_filters=self.neurons, filter_size=self.filter)
 
-    def init_model(self, nb_filters, filter_size, grid_size=(40, 1)):
+        self.init_model(nb_filters=self.neurons, filter_size=self.filter, time_steps=time_steps)
+
+    def init_model(self, nb_filters=1, filter_size=(3, 1), grid_size=(40, 1), time_steps=1):
         """
         Initialisation of CNN model
 
         Args:
             nb_filters (int): The number of different filters to learn.
             filter_size: The filter size
-            grid_size: shape of input (time, gridx, gridy=1, 1)
+            grid_size: shape of input
         """
 
-        inputs = Input(shape=(grid_size[0], grid_size[1], 1))
+        inputs = Input(shape=(grid_size[0], 1, 1))
 
         # padding for input grid
         paddings = tf.constant([[0, 0], [1, 1], [0, 0], [0, 0]])
         # execution of reflection padding
         padding_first = Lambda(lambda t: tf.pad(t, paddings, "REFLECT"))(inputs)
 
-        conv2d_first = Conv2D(filters=nb_filters,
-                              kernel_size=filter_size,
-                              activation='relu',
-                              padding="valid")(padding_first)
+        conv_first = Conv2D(filters=nb_filters,
+                            kernel_size=filter_size,
+                            activation='relu',
+                            padding="valid")(padding_first)
 
-        padding_second = Lambda(lambda t: tf.pad(t, paddings, "REFLECT"))(conv2d_first)
+        padding_second = Lambda(lambda t: tf.pad(t, paddings, "REFLECT"))(conv_first)
 
-        conv2d_second = Conv2D(filters=1,
-                               kernel_size=filter_size,
-                               activation='hard_sigmoid',
-                               padding="valid")(padding_second)
+        conv_second = Conv2D(filters=1,
+                             kernel_size=filter_size,
+                             activation='hard_sigmoid',
+                             padding="valid")(padding_second)
 
         print(inputs)
         print(padding_first)
-        print(conv2d_first)
+        print(conv_first)
         print(padding_second)
-        print(conv2d_second)
+        print(conv_second)
 
         # input_shape. tuple does not include the sample axis
-        self.model = Model(inputs=inputs, outputs=conv2d_second)
+        self.model = Model(inputs=inputs, outputs=conv_second)
 
         opt = optimizers.Adadelta()
 
         self.model.compile(loss='mae', optimizer=opt, metrics=['mae'])
-
-    """
-    def padding2d(self, tensor, paddings=((1, 1), (0, 0))):
-        t_left = tf.manip.roll(tensor, paddings[0][0], axis=1)
-        t_right = tf.manip.roll(tensor, -paddings[0][1], axis=1)
-        t_up = tf.manip.roll(tensor, paddings[1][0], axis=2)
-        t_down = tf.manip.roll(tensor, -paddings[1][1], axis=2)
-        t_pad = tf.concat([t_left])
-        return tensor
-    """
 
     def predict(self, x_test):
         """
@@ -99,6 +90,7 @@ class CNN(NN):
                        epochs=self.epochs,
                        batch_size=self.batch,
                        validation_data=(x_val, y_val),
+                       shuffle=True,
                        callbacks=callbacks)
 
     def scale(self, array):
@@ -113,7 +105,11 @@ class CNN(NN):
         """
         Invert application of `scale`
         """
-        pass
+        array = np.reshape(array, (array.shape[0], array.shape[1]))
+
+        array_unscaled = scaler.inverse_transform(array)
+
+        return array_unscaled
 
     def split_ds(self, array):
         """
@@ -134,12 +130,15 @@ class CNN(NN):
         return train, test, val
 
     def analysis_scatter(self, ytest, ypred):
+        print(ytest.shape)
+        print(ypred.shape)
+
         shape = ypred.shape[0] * ypred.shape[1]
 
         rmse = np.sqrt(mean_squared_error(ytest.reshape(shape), ypred.reshape(shape)))
 
         m, b = np.polyfit(ytest.reshape(shape), ypred.reshape(shape), 1)
-        x = ytest.reshape(shape)
+        x = range(-22, 22, 1)
         yreg = np.add(np.multiply(m, x), b)
 
         print("plotting Results...")
@@ -158,8 +157,8 @@ class CNN(NN):
         ax.grid()
         ax.set_xlabel("Test")
         ax.set_ylabel("Prediction")
-        ax.set_xlim(0, 1)
-        ax.set_ylim(0, 1)
+        ax.set_xlim(-20, 20)
+        ax.set_ylim(-20, 20)
         print("\t saving figure...")
         plt.savefig("CNN_%ineurons_%ifilter_%ibatchsize_%iepochs_%itimesteps.png" %
                     (self.neurons, self.filter[0], self.batch, self.epochs, self.time_steps), dpi=400)
@@ -189,6 +188,10 @@ if __name__ == "__main__":
 
     # prediction
     ypred = cnn.predict(xtest)
+
+    # invert scaling
+    ypred = cnn.scale_invert(yscaler, ypred)
+    ytest = cnn.scale_invert(yscaler, ytest)
 
     # performance analysis
     cnn.analysis_scatter(ytest, ypred)
