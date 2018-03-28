58b009f2eae9b819f7443cae19ba4d3d086c32d5
darklefknight <darklefknight@web.de>
2018-03-28 13:36:27 +0200

    Method for reshaping data.

--- bdp_cnn/cmip5/datahandler.py
+++ bdp_cnn/cmip5/datahandler.py
@@ -48,6 +48,29 @@ class DataHandler(object):
 
         nc.close()
 
+    def shape(self,array,inverse=False):
+        """
+        reshapes an array from 3D to 2D. If inverse==True then reshaping is from 2D to 3D using standard
+        lat=192 and lon=96.
+
+        Args:
+            array: numpy array
+            inverse: bool
+
+        Returns:
+            reshaped numpy array
+        """
+
+        #set data shape:
+        lat = 192
+        lon = 96
+
+        if not inverse:
+            return np.reshape(array,(array.shape[0],array.shape[1]*array.shape[2]))
+
+        else:
+            return np.reshape(array,(array.shape[0],lat,lon))
+
 
 if __name__ == "__main__":
     dh = DataHandler()
