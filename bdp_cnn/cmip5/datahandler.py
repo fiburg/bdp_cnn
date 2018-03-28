4ecd9eac2397f59e7ab501d138fd23f6601bfc78
fiburg <finn.burgemeister@gmail.com>
2018-03-28 13:42:15 +0200

    add methods to get variables and dimensions like time, ...

--- bdp_cnn/cmip5/datahandler.py
+++ bdp_cnn/cmip5/datahandler.py
@@ -43,14 +43,16 @@ class DataHandler(object):
         lon = nc.variables['lon'][:]
 
         todate = np.vectorize(datetime.strptime)
-        newtime = todate(time.astype('str'), '%Y%m%d')
+        newtime = todate(time.astype('int').astype('str'), '%Y%m%d')
         print(newtime)
 
         nc.close()
+
+        return time, lat, lon
 
 
 if __name__ == "__main__":
     dh = DataHandler()
 
     #test = dh.get_var('./data/lkm0401_echam6_BOT_mm_1850-2005.nc', 'var167')
-    dh.get_dims('./data/lkm0401_echam6_BOT_mm_1850-2005.nc')
+    #dh.get_dims('./data/lkm0401_echam6_BOT_mm_1850-2005.nc')
